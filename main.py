5ec867e29d8b0bf32f98c50e86d4d4076d526d29
Luiza <64799018+LuizaAlanis@users.noreply.github.com>
2022-11-23 17:11:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests as requests
 
 # TODO: Generate Token - https://bit.ly/3V8RP52
-TOKEN = 'xyz'
+TOKEN = 'TOKEN'
 
 
-def upload_package(package):
+def upload_apk(apk_path):
     api = "https://us-kcs-api.samsungknox.com/kcs/v1/kc/assets"
 
     if TOKEN is None:
@@ -15,16 +15,17 @@ def upload_package(package):
         'Content-Type': 'multipart/form-data'
     }
 
-    file = "path-and-filename-of-media-file"
     # REQUEST INFO - from https://bit.ly/3GE7PHE
-    response = requests.post(f"{api}", headers=auth_header, timeout=10, files=file)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    with open(apk_path, 'rb') as filedata:
+        response = requests.post(f"{api}", headers=auth_header, timeout=500, files={'file': filedata})
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return f"{response.status_code}:  {response.text}"
 
 
 if __name__ == '__main__':
     # TODO: package validation, local and remote.
     for i in range(1, 2, 1):  # (initial,final but not included,gap)
-        upload_package(f"/home/luiza/Documents/app-rztech-debug{1}.apk")
+        response = upload_apk(f"/Users/luizzabuscka/Downloads/MobileApp-1.0.9.{i}.apk")
+        print(response)
